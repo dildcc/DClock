fd7707d3f309f7e5b528f34ea01090f0c9084901
dildcc <93020768+dildcc@users.noreply.github.com>
2022-06-12 08:12:34 +0800

    Update DClock.lls.js

--- DClock.lls.js
+++ DClock.lls.js
@@ -8,6 +8,7 @@
 //   | |__|   __|     |  |_____________    |  |_____________
 //   |_______|        |________________|   |________________|
 
+// DIL工作室 室长:DCC 制作
 
 // ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
@@ -16,12 +17,14 @@
 
 // ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
+
+
 //插件注册
 let PLUGIN_NAME = "DClock";      //插件名称
 let PLUGIN_DESCRIPTION = "DClock 给钟插件 [未授权禁止转载]";     //插件描述
 let VERSION = [1,0,0];      //插件版本号
 let Publishing_website = "https://minebbs.com";   //插件发布地址
-let AUTHOR = "DCC";       //插件作者
+let AUTHOR = "DIL工作室";       //插件作者
 
 //检查 LL 版本
 if(!ll.requireVersion(2, 3, 0)){
@@ -39,10 +42,28 @@ else{
     });
 };
 
+load();
 
 // 进服提示
 function LoadName(pl) {
-    pl.tell("欢迎进入服务器,要钟请输入/DClock,查看帮助请用/DCHelper")
-}    
+    pl.tell('欢迎进入服务器,要钟请输入/DClock,查看帮助请用/DCHelper');
+}  
+
+function command(pl, comm) {
+    mc.runcmdEx('give "' + pl.realName + '" clock');
+    pl.tell('[DClock] 完成！');
+    return false;
+}
 
 // 注册指令
+function load() {
+    mc.listen("onJoin", LoadName)
+    mc.regPlayerCmd('dclock', '获取一个钟', command, 0) // 给玩家一个钟
+}
+
+mc.regPlayerCmd("DCHelper","DClock 帮助",function(pl,args){
+    pl.tell("用/DClock来获取钟,BUG反馈去Github,QQ:2829000751") // 输出帮助文字
+    pl.tell(args[0])
+})
+
+// 制作完成
