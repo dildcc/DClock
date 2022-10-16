f6c4217db9b8a3264918c535f65683ae24b7ce8d
dildcc <93020768+dildcc@users.noreply.github.com>
2022-10-16 20:17:27 +0800

    Update DClock.lls.js

--- DClock.lls.js
+++ DClock.lls.js
@@ -9,12 +9,11 @@
 //   |_______|        |________________|   |________________|
 
 // DIL工作室 室长:DCC 制作
-// 感谢Planet工作室
 
 // ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 //LiteLoaderScript Dev Helper
-/// <reference path="C:\Users\douyi\Documents\Library\JS\Api.js" /> 
+/// <reference path="C:/Users/douyi/Documents/Library/JS/Api.js" /> 
 
 // ———————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
@@ -23,7 +22,7 @@
 //插件注册
 let PLUGIN_NAME = "DClock";      //插件名称
 let PLUGIN_DESCRIPTION = "DClock 给钟插件 [未授权禁止转载]";     //插件描述
-let VERSION = [1,2,0];      //插件版本号
+let VERSION = [1,3,0];      //插件版本号
 let Publishing_website = "https://minebbs.com";   //插件发布地址
 let AUTHOR = "DIL工作室";       //插件作者
 
@@ -44,25 +43,30 @@ else{
 };
 
 
-// 进服提示
-function LoadName(pl) {
-    pl.tell('欢迎进入服务器,要钟请输入/DClock');
-}  
+// NBT弄不掉的钟
 
-let playerdata_path = "./DClock/playerdata.json" //配置文件
+let nbtclock = NBT.parseSNBT(String.raw`{"Count":1b,"Damage":0s,"Name":"minecraft:clock","WasPickedUp":0b,"tag":{"minecraft:item_lock":2b,"minecraft:keep_on_death":1b}}`);
+
+// 进服提示
+mc.listen("onJoin",function(pl){
+    pl.sendToast("§aDClock","§b欢迎来到§d本服务器,输入/dclock§c获取钟")
+})  
+
+
+
+const PLUGINS_DIR  = "./plugins/DClock";//文件夹
+let playerdata_path = "./plugins/DClock/playerdata.json" //配置文件
 let playerdata = data.openConfig(playerdata_path, "json", "{}")
 
 
-mc.regPlayerCmd("dclock", "给钟", function (pl, id) { //注册玩家指令
+mc.regPlayerCmd("dclock", "§b给§a一§c个§d钟", function (pl, id) { //注册玩家指令
     playerdata.reload() //重载配置文件
     var player = playerdata.get(pl.realName) //读取配置文件
     if (player == null){
         playerdata.set(pl.realName,1)
-        mc.runcmdEx("give " + pl.realName + "colock")
-        pl.tell("[DClock]给钟完成")
+        pl.giveItem(mc.newItem(nbtclock))
+        pl.tell("§6[DClock]§b给钟§d完成")
     } else {
-        pl.tell("[DClock]你已经获取了钟,无法重新获取")
+        pl.tell("§6[DClock]§b你已经获取了钟,§d无法重新获取")
     }
 })
-
-//制作完成
